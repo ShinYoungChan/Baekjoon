0c3b59a95c91129fc598927a0ada29754bf5d6d9
ShinYoungChan <40080826+ShinYoungChan@users.noreply.github.com>
2023-08-01 14:59:23 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int num;
+	scanf("%d", &num);
+
+	for (int i = 0; i < num; i++) {
+		for (int j = num; j > i + 1; j--)
+			printf(" ");
+		for (int j = 0; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+	return 0;
+}
