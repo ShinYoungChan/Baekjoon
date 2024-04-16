46918c433d665f7e7b317b139353e1ace6e21b41
ShinYoungChan <40080826+ShinYoungChan@users.noreply.github.com>
2024-04-16 13:55:32 +0900

    [Gold III] Title: 우주 탐사선, Time: 524 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/17182. 우주 탐사선/우주 탐사선.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int map[10][10];
+int N, K;
+int ans = 2100000000;
+
+void floyd() {
+	for (int k = 0; k < N; k++) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (map[i][k] + map[k][j] < map[i][j])
+					map[i][j] = map[i][k] + map[k][j];
+			}
+		}
+	}
+}
+
+void dfs(unordered_set<int> us, int sum, int idx) {
+	if (us.size() == N) {
+		if (ans > sum) ans = sum;
+		return;
+	}
+	for (int i = 0; i < N; i++) {
+		if (us.count(i) == 0) {
+			us.insert(i);
+			dfs(us, sum + map[idx][i], i);
+			us.erase(i);
+		}
+	}
+}
+
+int main() {
+	cin >> N >> K;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> map[i][j];
+		}
+	}
+	unordered_set<int> us;
+	us.insert(K);
+	floyd();
+	dfs(us, 0, K);
+	printf("%d\n", ans);
+	return 0;
+}
