dcddf0238d3be0eca97a9fe94db63559c29fdfeb
ShinYoungChan <40080826+ShinYoungChan@users.noreply.github.com>
2024-04-30 17:37:50 +0900

    [Gold V] Title: 꿀 따기, Time: 40 ms, Memory: 3196 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.cc
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.cc
@@ -7,9 +7,14 @@ int main() {
 	int n;
 	cin >> n;
 	vector<int> v(n), bee(n), backbee(n);
+	int asd = 0, idx = -1;
 	for (int i = 0; i < n; i++) {
 		cin >> v[i];
 		backbee[i] = bee[i] = v[i];
+		if (asd < v[i]) {
+			asd = v[i];
+			idx = i;
+		}
 	}
 	for (int i = 1; i < n; i++) {
 		bee[i] = bee[i] + bee[i - 1];
@@ -27,17 +32,14 @@ int main() {
 		int sum = (backbee[0] - v[i] - v[v.size() - 1]) + (backbee[0] - backbee[i + 1] - v[i]);
 		if (max < sum)max = sum;
 	}
-	// 꿀통 위치
-	for (int i = 1; i < v.size() - 1; i++) {
-		int sum = 0;
-		for (int j = 1; j <= i; j++) {
-			sum += v[j];
-		}
-		for (int j = v.size() - 2; j >= i; j--) {
-			sum += v[j];
-		}
-		if (max < sum)max = sum;
+	int sum = 0;
+	for (int i = 1; i <= idx; i++) {
+		sum += v[i];
 	}
+	for (int i = v.size() - 2; i >= idx; i--) {
+		sum += v[i];
+	}
+	if (max < sum)max = sum;
 	printf("%d\n", max);
 	return 0;
 }
