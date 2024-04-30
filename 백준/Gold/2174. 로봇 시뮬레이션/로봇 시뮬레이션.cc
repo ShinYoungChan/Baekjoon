409adb6ec9928d96bc58c27cb4cd034d7b87e2cf
ShinYoungChan <40080826+ShinYoungChan@users.noreply.github.com>
2024-04-30 16:27:38 +0900

    [Gold V] Title: 로봇 시뮬레이션, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/2174. 로봇 시뮬레이션/로봇 시뮬레이션.cc
+++ 백준/Gold/2174. 로봇 시뮬레이션/로봇 시뮬레이션.cc
@@ -1,119 +1,94 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
- 
-int di[] = { -1,0,1,0 };
-int dj[] = { 0,1,0,-1 };
- 
-//가로,세로,로봇 개수,명령 개수
-int A, B, N, M;
- 
-//로봇이 움직이는 맵
-int map[102][102];
-//로봇마다 정보(행,열,방향)
-int robotInfo[101][3];
- 
-bool crash = false;
- 
-bool simulation(int id, char cmd, int cmdCnt) {
- 
-    //명령내릴 로봇 위치,방향 저장
-    int tmpi = robotInfo[id][0];
-    int tmpj = robotInfo[id][1];
-    int tmpDir = robotInfo[id][2];
- 
-    map[tmpi][tmpj] = 0;
- 
-    //명령 반복 횟수만큼 반복
-    while (cmdCnt--) {
-        if (cmd == 'L') {
-            if (tmpDir == 0) {
-                tmpDir = 3;
-            }
-            else {
-                tmpDir--;
-            }
-        }
-        else if (cmd == 'R') {
-            if (tmpDir == 3) {
-                tmpDir = 0;
-            }
-            else {
-                tmpDir++;
-            }
-        }
-        else {
-            tmpi += di[tmpDir];
-            tmpj += dj[tmpDir];
- 
-            //벽에 충돌한 경우
-            if (tmpi <= 0 || tmpj <= 0 || tmpi > A || tmpj > B) {
-                cout << "Robot " << id << " crashes into the wall" << "\n";
-                return true;
-            }
- 
-            //다른 로봇에 충돌한 경우
-            if (map[tmpi][tmpj] != 0) {
-                cout << "Robot " << id << " crashes into robot " << map[tmpi][tmpj] << "\n";
-                return true;
-            }
-        }
-    }
- 
-    //충돌이 발생하지 않았을 경우, 로봇위치 변경
-    map[tmpi][tmpj] = id;
- 
-    robotInfo[id][0] = tmpi;
-    robotInfo[id][1] = tmpj;
-    robotInfo[id][2] = tmpDir;
- 
-    return false;
+
+class Robot {
+public:
+	int x, y, dir;
+public:
+	Robot() { x = y = dir = 0; }
+};
+
+int map[100][100];
+int dxdy[4][2] = { {-1,0},{0,1},{1,0},{0,-1} };	// 상 우 하 좌
+int a, b, n, m, flag;
+vector<Robot> v;
+
+void Init() {
+	cin >> b >> a;
+	cin >> n >> m;
+	v.resize(n + 1);
+	for (int i = 1; i <= n; i++) {
+		int x, y;
+		char ch;
+		cin >> y >> x >> ch;
+		x = a - x;
+		y = y - 1;
+		v[i].x = x, v[i].y = y;
+		map[x][y] = i;
+		if (ch == 'N') v[i].dir = 0;
+		else if (ch == 'E')v[i].dir = 1;
+		else if (ch == 'S')v[i].dir = 2;
+		else if (ch == 'W')v[i].dir = 3;
+	}
 }
- 
+
+
+void robotMove(int r, int cnt) {
+	int x = v[r].x;
+	int y = v[r].y;
+	int dir = v[r].dir;
+	map[x][y] = 0;
+	for (int i = 0; i < cnt; i++) {
+		x = x + dxdy[dir][0];
+		y = y + dxdy[dir][1];
+		// 로봇이 맵 밖을 벗어나는 경우
+		if (x<0 || x>a - 1 || y<0 || y>b - 1) {
+			printf("Robot %d crashes into the wall\n", r);
+			flag = 1;
+			return;
+		}
+		// 로봇과의 충돌이 발생하는 경우
+		if (map[x][y] != 0) {
+			printf("Robot %d crashes into robot %d\n", r, map[x][y]);
+			flag = 1;
+			return;
+		}
+	}
+	v[r].x = x;
+	v[r].y = y;
+	map[x][y] = r;
+}
+
+void robotRotate(int r, char type, int cnt) {
+	int dir = v[r].dir;
+	if (type == 'R') {
+		for (int i = 0; i < cnt; i++) {
+			dir = (dir + 1) % 4;
+		}
+	}
+	else {
+		for (int i = 0; i < cnt; i++) {
+			dir--;
+			if (dir < 0) dir = 3;
+		}
+	}
+	v[r].dir = dir;
+}
+
 int main() {
- 
-    //1.입력
-    cin >> B >> A >> N >> M;
-    for (int i = 1; i <= N; i++) {
-        int tmpi, tmpj, tmpDir;
-        char tmp;
- 
-        cin >> tmpj >> tmpi >> tmp;
- 
-        if (tmp == 'N') {
-            tmpDir = 0;
-        }
-        else if (tmp == 'E') {
-            tmpDir = 1;
-        }
-        else if (tmp == 'S') {
-            tmpDir = 2;
-        }
-        else {
-            tmpDir = 3;
-        }
- 
-        //배열index 조정
-        robotInfo[i][0] = (A + 1) - tmpi;
-        robotInfo[i][1] = tmpj;
-        robotInfo[i][2] = tmpDir;
- 
-        map[robotInfo[i][0]][robotInfo[i][1]] = i;
-    }
-    
-    //2.명령 수행
-    for (int i = 1; i <= M; i++) {
-        int id, cmdCnt;
-        char cmd;
-        cin >> id >> cmd >> cmdCnt;
- 
-        if (!crash) {
-            crash = simulation(id, cmd, cmdCnt);
-        }
-    }
- 
-    if (!crash) {
-        cout << "OK" << "\n";
-    }
- 
-    return 0;
+	Init();
+	for (int i = 0; i < m; i++) {
+		int r, cnt;
+		char type;
+		cin >> r >> type >> cnt;
+		if (type == 'F') robotMove(r, cnt);
+		else robotRotate(r, type, cnt);
+		if (flag) break;
+	}
+	if (!flag) {
+		printf("OK\n");
+	}
+	return 0;
 }
