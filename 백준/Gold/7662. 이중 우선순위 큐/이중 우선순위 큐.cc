8b0e7b266641691074d946cdd2d0c79639c69f29
ShinYoungChan <40080826+ShinYoungChan@users.noreply.github.com>
2024-07-29 15:54:16 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2348 ms, Memory: 52692 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+priority_queue<int> maxQ;
+priority_queue<int, vector<int>, greater<int>> minQ;
+unordered_map<int, int> um;
+
+void EnQueue(int num) {
+	maxQ.push(num);
+	minQ.push(num);
+	um[num]++;
+}
+
+void OutQueue(int num) {
+	if (num == 1) {
+		if (!maxQ.empty()) {
+			um[maxQ.top()]--;
+			maxQ.pop();
+		}
+	}
+	else {
+		if (!minQ.empty()) {
+			um[minQ.top()]--;
+			minQ.pop();
+		}
+	}
+}
+
+int main() {
+	int test;
+	cin >> test;
+
+	for (int t = 0; t < test; t++) {
+		int k;
+		cin >> k;
+
+		while (!maxQ.empty())maxQ.pop();
+		while (!minQ.empty())minQ.pop();
+		um.clear();
+		for (int i = 0; i < k; i++) {
+			char ch;
+			int num;
+			cin >> ch >> num;
+			if (ch == 'I') {
+				EnQueue(num);
+			}
+			else {
+				OutQueue(num);
+				while (!maxQ.empty() && um[maxQ.top()] == 0) maxQ.pop();
+				while (!minQ.empty() && um[minQ.top()] == 0) minQ.pop();
+			}
+		}
+		while (!maxQ.empty() && um[maxQ.top()] == 0) maxQ.pop();
+		while (!minQ.empty() && um[minQ.top()] == 0) minQ.pop();
+
+		if (maxQ.empty() || minQ.empty()) cout << "EMPTY\n";
+		else cout << maxQ.top() << " " << minQ.top() << "\n";
+	}
+	return 0;
+}
